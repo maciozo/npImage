0d2775b534f8b2a591c1e870419cf8a1cc282fb2
Unazed Spectaculum <leethax0r.coder@gmail.com>
2017-09-21 15:19:46 +0100

    pep8 additions
    haven't really tested this but there aren't any syntax errors, perhaps some logic errors but the code seems functional from the way I edited it and reviewed it.
    fixed pep8 mistakes e.g. function names, variable names and improved some bits here and there

--- sendNP.py
+++ sendNP.py
@@ -1,18 +1,24 @@
-import pysftp
+import json
 import os
+
+try:
+    import pysftp
+except ImportError:
+    print("{ERROR} You need the 'PySFTP' module in order to run this program!")
+    exit(1)
+
 import time
 import traceback
-import json
-import sys
 
 try:
     import getpass
     has_getpass = True
-except:
+except ImportError:
     has_getpass = False
-    
+
+
 config = {}
-defaultConfig = """{
+default_config = """{
     "imgPath" : "./image.png",
     "textPath" : "./np.txt",
     "font" : "arial.ttf",
@@ -37,23 +43,26 @@ defaultConfig = """{
     "SFTPscanInterval" : 10
 }"""
 
-def getConfig():
-    global defaultConfig, config
-    confFile = "./npImage.json"
-    if not os.path.isfile(confFile):
-        cf = open(confFile, "w", encoding = "utf-8")
-        cf.write(defaultConfig)
-        cf.close()
-        print("Generated new config file (npImage.json)")
-        sys.exit(0)
-    cf = open(confFile, "r", encoding = "utf-8")
-    config = json.loads(cf.read())
-    checkConfig()
-    cf.close()
-    
-def checkConfig():
+def get_config():
+    global default_config
     global config
-    expectedTypes = {
+    
+    conf_file = "./npImage.json"
+    
+    if not os.path.isfile(conf_file):
+        with open(conf_file, "w", encoding="utf-8") as cf:
+            cf.write(default_config)
+        print("Generated new config file (npImage.json)")
+        exit(1)
+        
+    with open(conf_file, encoding="utf-8") as cf:
+        config = json.loads(cf.read())
+        check_config()
+    
+def check_config():
+    global config
+    
+    expected_types = {
                         "imgPath" : str,
                         "textPath" : str,
                         "font" : str,
@@ -78,55 +87,57 @@ def checkConfig():
                         "SFTPscanInterval" : int
                     }
                     
-    for option, expectedType in expectedTypes.items():
+    for option, expected_type in expected_types.items():
         try:
-            if (type(config[option]) != expectedType):
-                print("Invalid %s. Expected type %s, got %s." % (option, str(expectedType), str(type(config[option]))))
-                sys.exit(1)
+            if not isinstance(config[option], expected_type):
+                print("Invalid %s. Expected type %s, got %s." % (option, expected_type, type(config[option])))
+                exit(1)
         except KeyError:
             print("Missing option: %s" % option)
-            sys.exit(1)
+            exit(1)
             
     for channel in config["fontColour"]:
-        if (type(channel) != int):
-            print("Invalid fontColour value: %s. Expected type %s, got %s." % (str(channel), str(int), str(type(channel))))
-            sys.exit(1)
+        if not isinstance(channel, int):
+            print("Invalid fontColour value: %s. Expected type %s, got %s." % (channel, int, type(channel)))
+            exit(1)
             
     for channel in config["backgroundColour"]:
-        if (type(channel) != int):
-            print("Invalid backgroundColour value: %s. Expected type %s, got %s." % (str(channel), str(int), str(type(channel))))
-            sys.exit(1)
+        if not isinstance(channel, int):
+            print("Invalid backgroundColour value: %s. Expected type %s, got %s." % (channel, int, type(channel)))
+            exit(1)
             
     for channel in config["blankImageSize"]:
-        if (type(channel) != int):
-            print("Invalid blankImageSize value: %s. Expected type %s, got %s." % (str(channel), str(int), str(type(channel))))
-            sys.exit(1)
+        if not isinstance(channel, int):
+            print("Invalid blankImageSize value: %s. Expected type %s, got %s." % (channel, int, type(channel)))
+            exit(1)
             
     for channel in config["blankImageBackground"]:
-        if (type(channel) != int):
-            print("Invalid blankImageBackground value: %s. Expected type %s, got %s." % (str(channel), str(int), str(type(channel))))
-            sys.exit(1)
+        if not isinstance(channel, int):
+            print("Invalid blankImageBackground value: %s. Expected type %s, got %s." % (channel, int, type(channel)))
+            exit(1)
             
     for channel in config["blankImageText"]:
-        if (type(channel) != str):
-            print("Invalid blankImageText value: %s. Expected type %s, got %s." % (str(channel), str(str), str(type(channel))))
-            sys.exit(1)
+        if not isinstance(channel, basestring):
+            print("Invalid blankImageText value: %s. Expected type %s, got %s." % (channel, str, type(channel)))
+            exit(1)
             
     for channel in config["blankImageFontColour"]:
-        if (type(channel) != int):
-            print("Invalid blankImageFontColour value: %s. Expected type %s, got %s." % (str(channel), str(int), str(type(channel))))
-            sys.exit(1)
+        if not isinstance(channel, int):
+            print("Invalid blankImageFontColour value: %s. Expected type %s, got %s." % (channel, int, type(channel)))
+            exit(1)
     
 
 def main():
     global config
-    getConfig()
+    
+    get_config()
+    
     if has_getpass:
         password = getpass.getpass("Password for %s@%s: " % (config["username"], config["hostname"]))
     else:
         password = input("Password for %s@%s (WILL BE VISIBLE - USE GETPASS): " % (config["username"], config["hostname"]))
             
-    while (1):
+    while 1:
         try:
             oldmt = os.path.getmtime(config["imgPath"])
             break
@@ -135,13 +146,14 @@ def main():
             time.sleep(config["SFTPscanInterval"])
             
     
-    sendFile(password)
+    send_file(password)
     mainloop(oldmt, password)
     
 def mainloop(oldmt, password):
     global config
-    while(1):
-        while (1):
+    
+    while 1:
+        while 1:
             try:
                 mt = os.path.getmtime(config["imgPath"])
                 break
@@ -149,16 +161,18 @@ def mainloop(oldmt, password):
                 print("Cannot find file: %s" % config["imgPath"])
                 time.sleep(config["SFTPscanInterval"])
         # Check if modification time has changed
-        if (mt > oldmt):
+        if mt > oldmt:
             print("File has changed. %d > %d" % (mt, oldmt))
-            sendFile(password)
+            send_file(password)
             oldmt = mt
         time.sleep(config["SFTPscanInterval"])
             
-def sendFile(password):
+def send_file(password):
     global config
+    
     cnopts = pysftp.CnOpts()
     cnopts.hostkeys = None
+    
     try:
         with pysftp.Connection(config["hostname"], username = config["username"], password = password, port = config["port"], cnopts = cnopts) as sftp:
             sftp.chdir(config["remoteDir"])
